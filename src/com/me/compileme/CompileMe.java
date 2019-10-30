b743c5cd2da382bd2f7dc05f38a8f3b56501f82c
venmanda <mvswathi446@gmail.com>
2019-10-30 21:15:07 +0530

    My first commit

---
+++ src/com/me/compileme/CompileMe.java
@@ -0,0 +1,12 @@
+package com.me.compileme;
+
+public class CompileMe {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("*********Welcome to Jenkins Training **********");
+
+	}
+
+}
